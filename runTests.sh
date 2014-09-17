a79f9bd218b4004712de4eb4ee7eff96bc6cab09
Burak Yavuz <brkyvz@gmail.com>
2014-09-16 17:25:32 -0700

    Fixed test parameters

--- runTests.sh
+++ runTests.sh
@@ -4,16 +4,16 @@ export NUM_TRIALS=0
 export WARM_UP=0
 export WAIT=0
 export SEED=24
-export NUM_ROWS=( 1000000 10000000 100000000 )
-export NUM_COLS=( 100 1000 10000 )
-export SPARSITY=( 0.01 0.05 0.10 0.20 )
+export NUM_ROWS=( 1000000 10000000 )
+export NUM_COLS=( 500 1000 5000 10000 )
+export SPARSITY=( 0.01 0.05 0.10 0.15 0.20 )
 export ITER=2
 export STEP=4
 export REG=4
 export PARTS=64
 
 export MC=1000000
-export NC=100
+export NC=500
 export SP=0.05
 
 for ROWS in ${NUM_ROWS[@]}; do
@@ -48,10 +48,10 @@ for RHO in ${SPARSITY[@]}; do
 done
 
 export NUM_COLS=( 10000 100000 1000000 )
-export NUM_ROWS=( 100 1000 10000 100000 )
+export NUM_ROWS=( 500 1000 5000 10000 50000 )
 
 export NC=100000
-export MC=1000
+export MC=500
 
 
 for ROWS in ${NUM_ROWS[@]}; do
